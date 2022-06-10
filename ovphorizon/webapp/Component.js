68a6584eb2350d89d0e8159f91543929b31149ad
Tristan <tristan.heisecke@implico.com>
2022-06-10 13:26:43 +0200

    create ovp in horizon style

---
+++ ovphorizon/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/ovp/app/Component"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("demp.opvhorizon.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
